c1c1f9fd331f7db77606d87dae7f22d416c8a6d9
Eashan Goel <eashan2@gmail.com>
2022-09-01 00:09:00 +0530

    added the localStorage for project buttons

--- src/domManip.js
+++ src/domManip.js
@@ -1,3 +1,10 @@
+class newButton{
+    constructor(projectName, projectID){
+        this.projectName= projectName;
+        this.projectID= projectID;
+    }
+}
+
 export default function addElement(type, classname, addTo=null, srcEle= null, id=null, text=null){
     let ele = document.createElement(type);
     ele.classList.add(classname);
@@ -15,4 +22,16 @@ export default function addElement(type, classname, addTo=null, srcEle= null, id
     }
     
     return ele;
+}
+
+export function putButtonInLocalStorage(name){
+    let a= JSON.parse(localStorage.getItem("sideBarButtonsArray"));
+    let b= parseInt(localStorage.getItem('sideBarButtonsId'));
+    b+=1;
+    localStorage.setItem('sideBarButtonsId', b);
+    let nextButton = new newButton(name, b)
+    a[b]= nextButton;
+    localStorage.setItem("sideBarButtonsArray",JSON.stringify(a));
+    console.log(JSON.parse(localStorage.getItem("sideBarButtonsArray")));
+    return nextButton;
 }
--- src/home.js
+++ src/home.js
@@ -1,18 +1,19 @@
 import logoLoc from './logo.png'
 import addElement from './domManip.js';
+import { putButtonInLocalStorage } from './domManip.js';
 import plusloc from './plus-thick-2.png'
-
-let numButton = 0;
-let sideBarButtonsArray= [];
+let backEndSideBarButtonsArray= [];
 let divToReturn = addElement('div', 'mainHomeDiv');
 let headerDiv= addElement('div', 'headerDiv', divToReturn);
 let sideBarDiv= addElement('div', 'sidebarDiv', divToReturn);
 let logo= addElement('img', 'mainLogo', headerDiv, logoLoc);
 let listsHeaderDiv= addElement('div', 'listsHeaderDiv', sideBarDiv)
 let buttonList = addElement('div', 'button-list', sideBarDiv);
-let allButton= addElement('button', 'buttonsInList', buttonList, undefined, numButton++, "All");
-let arr= [];
-sideBarButtonsArray.push(arr);
+// let sideBarButtonsArray= [];
+// localStorage.setItem("sideBarButtonsArray",JSON.stringify(sideBarButtonsArray));
+// localStorage.setItem('sideBarButtonsId',0);
+console.log(JSON.parse(localStorage.getItem("sideBarButtonsArray")));
+let allButton= addElement('button', 'buttonsInList', buttonList, undefined, 0, "All");
 let addButtonDiv= addElement('div', 'addButtonDiv', sideBarDiv)
 let addButton= addElement('img', 'addButton', addButtonDiv, plusloc);
 let listsHeader = addElement('p', 'listsHeader', listsHeaderDiv);
@@ -23,16 +24,36 @@ export default function addHomeDOM(){
     return divToReturn;
 }
 function showAddingProjectButton(){
-    let addingDiv= addElement('div', 'addingProjectDiv', buttonList);
+    let addingDiv= addElement('div', 'projectAdder', buttonList);
     let newInput = addElement('input', 'addingProjectInput', addingDiv);
-    let yesButton = addElement('button', 'addingProjectButton', addingDiv, undefined, undefined, "Add");
-    let noButton = addElement('button', 'addingProjectButton', addingDiv, undefined, undefined, "Cancel");
+    let yesButton = addElement('button', 'addingProjectButton', addingDiv, undefined, undefined, 'Add');
+    let noButton = addElement('button', 'addingProjectButton', addingDiv, undefined, undefined, 'Cancel');
+
+    noButton.addEventListener('click', (event)=>{
+        addingDiv.remove();
+    })
+    
+    yesButton.addEventListener('click', (event)=>{
+        if (newInput.value==""){
+            newInput.placeholder= "Please name your project"
+        }
+        else{
+        let buttonObject = putButtonInLocalStorage(newInput.value);
+        backEndSideBarButtonsArray.push(buttonObject);
+        let newButton = addElement('button', 'buttonsInList', buttonList, undefined, buttonObject.projectID, newInput.value);
+        console.log(JSON.parse(localStorage.getItem("sideBarButtonsArray")));
+        addingDiv.remove();}
+    })
 }
 
-// addButton.addEventListener('click', function addButtonToList(){
-//     let newButton = addElement('button', 'buttonsInList', buttonList, undefined, numButton++, "new");
-//     let arr =[];
-//     sideBarButtonsArray.push(arr);
-//     showAddingProjectButton();
-// })
+addButton.addEventListener('click', function addButtonToList(){
+    let temp = document.getElementsByClassName('projectAdder');
+    console.log(temp.length)
+    if (temp.length !== 0){
+        return 0;
+    }
+    else {
+    showAddingProjectButton();
+    }
+})
 
